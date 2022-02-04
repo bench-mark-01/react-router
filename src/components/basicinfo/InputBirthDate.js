ebddcca7362997015dd507d6af5ddd4141b68390
bench-mark-01 <63197635+bench-mark-01@users.noreply.github.com>
2022-02-04 10:45:11 +0900

    update input birth date

--- src/components/basicinfo/InputBirthDate.js
+++ src/components/basicinfo/InputBirthDate.js
@@ -3,55 +3,86 @@ import React, { useState, useEffect } from "react";
 export function InputBirthDate(){
     const today = new Date();
     const thisYear = today.getFullYear();
-    const birthYear = [];
-    const birthMonth = [];
-    const birthDate = [];
+    const oldYear = thisYear - 80;
 
-    for(let i = thisYear - 80; i<= thisYear; i++){
-        birthYear.push(i);
-    }
-    for(let i = 1; i<= 12; i++){
-        birthMonth.push(i);
-    }
-    for(let i = 1; i<= 31; i++){
-        birthDate.push(i);
-    }
+    const yearArray = [];
+    const monthArray = [];
+    let days_28 = [];
+    let days_29 = [];
+    let days_30 = [];
+    let days_31 = [];
 
-    const [userSelectYear, setUserSelectYear] = useState();
-    const [userSelectMonth, setUserSelectMonth] = useState();
-    const [userSelectDate, setUserSelectDate] = useState();
+    const [birthYear, setBirthYear] = useState([]);
+    const [birthMonth, setBirthMonth] = useState([]);
+    const [birthDate, setBirthDate] = useState([]);
+
+    const [userSelectYear, setUserSelectYear] = useState(oldYear);
+    const [userSelectMonth, setUserSelectMonth] = useState(1);
+    const [userSelectDate, setUserSelectDate] = useState(1);
 
     const handleChange = e => {
         if(e){
             if(e.target.name === 'year'){
                 let year = Number(e.target.value);
-                console.log(typeof(year));
                 setUserSelectYear(year);
             }
             if(e.target.name === 'month'){
-                setUserSelectMonth(e.target.value);
+                let month = Number(e.target.value);
+                setUserSelectMonth(month);
             }
             if(e.target.name === 'date'){
-                setUserSelectDate(e.target.value);
+                let date = Number(e.target.value);
+                setUserSelectDate(date);
             }
         }
         else{
             console.error('undefind');
         }
-    }
+    };
 
-    useEffect((userSelectYear) =>{
-            //処理
-            console.log(userSelectYear);
-            if(userSelectYear % 400 === 0){
-                return console.log(true);
-                if(year % 4 === 0 && year % 100 !== 0){
-                    return console.log(true);
-                }
-            }else{
-                return console.log(false);
+    useEffect(() =>{
+        for(let i = oldYear; i<= thisYear; i++){
+            yearArray.push(i);
+        }
+        setBirthYear(yearArray);
+        
+        for(let i = 1; i<= 12; i++){
+            monthArray.push(i);
+        }
+        setBirthMonth(monthArray);
+        
+        for(let i = 1; i<= 31; i++){
+            days_31.push(i);
+        }
+        for(let i = 1; i<= 30; i++){
+            days_30.push(i);
+        }
+        for(let i = 1; i<= 29; i++){
+            days_29.push(i);
+        }
+        for(let i = 1; i<= 28; i++){
+            days_28.push(i);
+        }
+        setBirthDate(days_31);
+    },[])
+
+    useEffect(() =>{
+        const lastDay = new Date(userSelectYear, userSelectMonth, 0).getDate();
+        switch (lastDay){
+            case 28:{
+                setBirthDate(days_28);
             }
-    },[userSelectYear]);
+            case 29:{
+                setBirthDate(days_29);
+            }
+            case 30:{
+                setBirthDate(days_30);
+            }
+            case 31:{
+                setBirthDate(days_31);
+            }
+        }
+    },[userSelectMonth]);
 
     return(
         <>
@@ -78,7 +109,7 @@ export function InputBirthDate(){
                 <select
                         name = 'month'
                         onChange={handleChange}
-                    >
+                        >
                         {birthMonth.map((month) =>{
                             return(
                                 <option key={'Month:' + month} value={month}>
@@ -93,7 +124,7 @@ export function InputBirthDate(){
                 <select
                         name = 'date'
                         onChange={handleChange}
-                    >
+                        >
                         {birthDate.map((date) =>{
                             return(
                                 <option key={'Date:' + date} value={date}>
@@ -102,13 +133,10 @@ export function InputBirthDate(){
                             )     
                         })}
                     </select>
-                    月
+                    日
                 </label>
 
             </form>
-
         </>
-
     )
-
 }
